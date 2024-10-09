cf56b22da973b7c50dea7ef085948741e8ffd16d
Vafin Artem <a.vafin85@gmail.com>
2024-10-09 11:32:08 +0500

    Добавляет слайдер в компонент Promo

---
+++ app/training/components/promo/components/slider/slider.tsx
@@ -0,0 +1,49 @@
+import React, {ReactElement} from "react";
+import {SliderProps} from "./slider.props";
+import styles from "./slider.module.css";
+import {Autoplay, Pagination} from "swiper/modules";
+import {Swiper, SwiperSlide} from "swiper/react";
+import clsx from "clsx";
+import Slide from "@/app/training/components/promo/components/slide/slide";
+
+const Slider = ({className}: SliderProps): ReactElement | null => {
+  return (
+    <Swiper
+      className={className}
+      modules={[Autoplay, Pagination]}
+      autoplay={{
+        delay: 4000,
+      }}
+      pagination={{
+        el: `.${styles.pagination}`,
+        bulletClass: styles.paginationItem,
+        bulletActiveClass: styles.active,
+        renderBullet: function (index, className) {
+          return `<span class="${className}"><span class="${styles.inner}"></span></span>`;
+        },
+      }}
+      spaceBetween={1}
+      slidesPerView={1}
+      onSlideChange={() => console.log("slide change")}
+      onAutoplayTimeLeft={function (swiper, timeLeft, percentage) {
+        const activeBullet = swiper.pagination.bullets[swiper.activeIndex];
+
+        if (!activeBullet) return;
+
+        (activeBullet.querySelector(`.${styles.inner}`)! as HTMLElement).style.width = `${
+          100 - percentage * 100
+        }%`;
+      }}
+    >
+      <SwiperSlide>
+        <Slide />
+      </SwiperSlide>
+      <SwiperSlide>
+        <Slide />
+      </SwiperSlide>
+      <div className={clsx(styles.pagination)}></div>
+    </Swiper>
+  );
+};
+
+export default Slider;
--- app/training/components/promo/promo.tsx
+++ app/training/components/promo/promo.tsx
@@ -1,7 +1,6 @@
 "use client";
 
 import React, {ReactElement} from "react";
-import {getImageProps} from "next/image";
 import {PromoProps} from "./promo.props";
 import styles from "./promo.module.css";
 import clsx from "clsx";
@@ -9,42 +8,9 @@ import Heading from "@/components/_tags/heading/heading";
 import Paragraph from "@/components/_tags/paragraph/paragraph";
 import Button from "@/components/_buttons/button/button";
 import Timer from "@/components/_common/timer/timer";
+import Slider from "@/app/training/components/promo/components/slider/slider";
 
 const Promo = ({className, icon, courseTypeName}: PromoProps): ReactElement | null => {
-  const common = {alt: "", quality: 95};
-  const {
-    props: {srcSet: desktop, ...rest},
-  } = getImageProps({
-    ...common,
-    width: 645,
-    height: 660,
-    priority: true,
-    src: "/img/components/promo/author-desktop.jpg",
-  });
-  const {
-    props: {srcSet: laptop},
-  } = getImageProps({
-    ...common,
-    width: 440,
-    height: 510,
-    src: "/img/components/promo/author-laptop.jpg",
-  });
-  const {
-    props: {srcSet: tablet},
-  } = getImageProps({
-    ...common,
-    width: 708,
-    height: 200,
-    src: "/img/components/promo/author-tablet.jpg",
-  });
-  const {
-    props: {srcSet: mobile},
-  } = getImageProps({
-    ...common,
-    width: 310,
-    height: 150,
-    src: "/img/components/promo/author-mobile.jpg",
-  });
 
   return (
     <section className={clsx(className, styles.wrapper)}>
@@ -69,21 +35,7 @@ const Promo = ({className, icon, courseTypeName}: PromoProps): ReactElement | nu
           <Timer text={`До повышения стоимости`} />
         </div>
       </div>
-      <div className={styles.sliderWrapper}>
-        <div className={styles.slide}>
-          <picture className={styles.picture}>
-            <source media="(min-width: 1500px)" srcSet={desktop} />
-            <source media="(max-width: 530px)" srcSet={mobile} />
-            <source media="(max-width: 1099px)" srcSet={tablet} />
-            <source media="(max-width: 1499px)" srcSet={laptop} />
-            <img {...rest} width={440} height={510} className={styles.image} />
-          </picture>
-          <p className={styles.speaker}>
-            <span className={styles.name}>Волкова Юлия Валерьевна</span>
-            <span className={styles.position}>Стоматолог-хирург, пародонтолог</span>
-          </p>
-        </div>
-      </div>
+      <Slider className={styles.sliderWrapper} />
     </section>
   );
 };
