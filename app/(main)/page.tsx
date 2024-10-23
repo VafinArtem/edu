21a70067a50521abf453b55395f89c0c11a16292
Vafin Artem <a.vafin85@gmail.com>
2024-10-23 10:54:56 +0500

    Правит структуру страниц

--- app/(main)/page.tsx
+++ app/(main)/page.tsx
@@ -5,10 +5,21 @@ export default function Home() {
   return (
     <ul className={`container`}>
       <li>
-        <Link href={Route.TRAINING} style={{textDecoration: `underline`}}>Один курс</Link>
+        <Link href={Route.TRAINING} style={{textDecoration: `underline`}}>Курсы</Link>
+      </li>
+      <li>
+        <Link href={`${Route.TRAINING}/preview`} style={{textDecoration: `underline`}}>Один курс - превью</Link>
       </li>
       <li>
         <Link href={Route.SPEAKER} style={{textDecoration: `underline`}}>Один преподаватель</Link>
+      </li>
+      <li>
+        <Link href={`${Route.TRAINING}/klinicheskaya-paradantalogya`} style={{textDecoration: `underline`}}>Карточка
+          1</Link>
+      </li>
+      <li>
+        <Link href={`${Route.TRAINING}/bazovyi-kurs-po-implantologii-ortopedicheskii-etap`}
+          style={{textDecoration: `underline`}}>Карточка 2</Link>
       </li>
     </ul>
   );
