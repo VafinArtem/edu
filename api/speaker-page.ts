22493328ce8896300ad8882e4184f693a8f00284
Vafin Artem <a.vafin85@gmail.com>
2024-11-19 13:16:22 +0500

    тестирует данные с бэка

--- api/speaker-page.ts
+++ api/speaker-page.ts
@@ -2,6 +2,7 @@ import {API} from "@/api/constants";
 import {SpeakerPageModel} from "@/interfaces/speaker";
 
 export async function getSpeakerPage(alias: string): Promise<{code: number, data: SpeakerPageModel} | null> {
+  console.log(`${API.speaker.byAlias}${process.env.NODE_ENV === "development" ? alias : ``}`);
   const res = await fetch(`${API.speaker.byAlias}${process.env.NODE_ENV === "development" ? alias : ``}`, {
     method: process.env.NODE_ENV === "development" ? "GET" : "POST",
     body: process.env.NODE_ENV === "development" ? null : JSON.stringify({alias}),
