1353328202ea0ec8d8dfadb1a06898e8576a45c2
Vafin Artem <a.vafin85@gmail.com>
2024-11-20 15:40:52 +0500

    готовит данные для получения страницы курсов

--- api/courses-page.ts
+++ api/courses-page.ts
@@ -24,9 +24,9 @@ export async function getCoursesPage(slug?: string[], searchParams?: Record<stri
     searchParams,
   };
 
-  console.log(JSON.stringify({query}));
+  console.log(JSON.stringify(query));
 
-  const body = process.env.NODE_ENV === "development" ? null : JSON.stringify({query});
+  const body = process.env.NODE_ENV === "development" ? null : JSON.stringify(query);
   const method = process.env.NODE_ENV === "development" ? "GET" : "POST";
 
   const res = await fetch(`${API.courses.byType}`, {
