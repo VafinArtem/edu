9dbf8c49712794dcb4b23e933069eafd52f54057
Vafin Artem <a.vafin85@gmail.com>
2024-11-26 12:51:46 +0500

    Правит страницу контактов и спикеров

--- app/(gray)/speakers/[[...slug]]/page.tsx
+++ app/(gray)/speakers/[[...slug]]/page.tsx
@@ -1,7 +1,15 @@
 import {getSpeakersPage} from "@/api/speakers-page";
 import SpeakersPage from "@/views/speakers-page/speakers-page";
+import {Metadata} from "next";
 import {notFound} from "next/navigation";
 import React, {ReactElement} from "react";
+
+export async function generateMetadata(): Promise<Metadata> {
+  return {
+    title: `Преподаватели центра - Учебный центр Амрита`,
+    description: ``,
+  };
+}
 
 const SpeakersLayout = async (): Promise<ReactElement | null> => {
   const page = await getSpeakersPage();
--- app/(white)/contacts/page.tsx
+++ app/(white)/contacts/page.tsx
@@ -1,6 +1,14 @@
 import YaMapLoader from "@/components/_location/ya-map-loader/ya-map-loader";
 import ContactsPage from "@/views/contacts-page/contacts-page";
+import {Metadata} from "next";
 import React, {ReactElement} from "react";
+
+export async function generateMetadata(): Promise<Metadata> {
+  return {
+    title: `Контакты - Учебный центр Амрита`,
+    description: ``,
+  };
+}
 
 const ContactsLayout = async (): Promise<ReactElement | null> => {
   return (
