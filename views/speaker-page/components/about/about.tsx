e336a4bc8fc144bb4790fdaf8026dc183e01e893
Vafin Artem <a.vafin85@gmail.com>
2024-12-16 14:23:16 +0500

    Правит компонент about

--- views/speaker-page/components/about/about.tsx
+++ views/speaker-page/components/about/about.tsx
@@ -1,14 +1,15 @@
-import React, {ReactElement} from "react";
-import {AboutProps} from "./about.props";
-import styles from "./about.module.css";
 import SectionItem from "@/components/_section/section-item/section-item";
 import Heading from "@/components/_tags/heading/heading";
+import Paragraph from "@/components/_tags/paragraph/paragraph";
+import React, {ReactElement} from "react";
+import styles from "./about.module.css";
+import {AboutProps} from "./about.props";
 
 const About = ({content, ...props}: AboutProps): ReactElement | null => {
   return (
     <SectionItem className={`container`} {...props}>
       <Heading tag={`h2`} className={styles.title}>О&nbsp;преподавателе</Heading>
-      <div className={styles.content} dangerouslySetInnerHTML={{__html: content}}></div>
+      <Paragraph fontWeight={"light"} className={styles.content}>{content}</Paragraph>
     </SectionItem>
   );
 };
