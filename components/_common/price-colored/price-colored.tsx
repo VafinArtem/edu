8d8ced1f0658e765eab8a8266c8a77394d95c255
Vafin Artem <a.vafin85@gmail.com>
2025-03-13 18:56:10 +0500

    Добавляет условие вывода ответа формы записи и цены

--- components/_common/price-colored/price-colored.tsx
+++ components/_common/price-colored/price-colored.tsx
@@ -1,14 +1,15 @@
-import React, {ReactElement} from "react";
-import {PriceColoredProps} from "./price-colored.props";
-import styles from "./price-colored.module.css";
-import clsx from "clsx";
 import {formatPrice} from "@/helpers/helpers";
+import clsx from "clsx";
+import React, {ReactElement} from "react";
+import styles from "./price-colored.module.css";
+import {PriceColoredProps} from "./price-colored.props";
 
 const PriceColored = ({price, oldPrice, className}: PriceColoredProps): ReactElement | null => {
   return (
     <p className={clsx(styles.wrapper, className)}>
       <span className="visually-hidden">Текущая цена</span>
-      {formatPrice(price)}&nbsp;₽
+      {Boolean(price) && <>{formatPrice(price)}&nbsp;₽</>}
+      {price === 0 && <>Бесплатно</>}
       {oldPrice && <>
         <span className="visually-hidden">Цена без скидки</span>
         <span className={styles.old}>
--- components/_common/price/price.tsx
+++ components/_common/price/price.tsx
@@ -1,14 +1,15 @@
-import React, {ReactElement} from "react";
-import {PriceProps} from "./price.props";
-import styles from "./price.module.css";
-import clsx from "clsx";
 import {formatPrice} from "@/helpers/helpers";
+import clsx from "clsx";
+import React, {ReactElement} from "react";
+import styles from "./price.module.css";
+import {PriceProps} from "./price.props";
 
 const Price = ({price, oldPrice, className}: PriceProps): ReactElement | null => {
   return (
     <p className={clsx(styles.wrapper, className)}>
       <span className="visually-hidden">Текущая цена</span>
-      {formatPrice(price)}&nbsp;₽
+      {Boolean(price) && <>{formatPrice(price)}&nbsp;₽</>}
+      {price === 0 && <>Бесплатно</>}
       {oldPrice && <>
         <span className="visually-hidden">Цена без скидки</span>
         <span className={styles.old}>
