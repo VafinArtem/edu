39a1a0ae871011116aaad5c052d8557053c09149
Vafin Artem <a.vafin85@gmail.com>
2025-03-25 18:36:57 +0500

    Правит страницу одной новости

--- app/(gray)/news/page.tsx
+++ app/(gray)/news/page.tsx
@@ -1,16 +1,30 @@
 import {getNewsPage} from "@/api/news-page";
 import {getTypes} from "@/api/news-types";
+import {getOneNewsPage} from "@/api/one-news-page";
 import NewsPage from "@/views/news-page/news-page";
+import {Metadata} from "next";
 import {notFound} from "next/navigation";
 import {ReactElement} from "react";
+
+export async function generateMetadata({params}: {params: {slug: string}}): Promise<Metadata> {
+  const page = await getOneNewsPage(params.slug);
+
+  if (!page || page.code !== 200) {
+    return {
+      title: `404`,
+    };
+  }
+
+  return {
+    title: `Новости учебного центра Amrita`,
+    description: ``,
+  };
+}
 
 const NewsLayout = async (props: {searchParams?: Promise<Record<string, string>>;}): Promise<ReactElement | null> => {
   const search = await props.searchParams;
   const page = await getNewsPage(search);
   const types = await getTypes();
-
-  console.log(page);
-  console.log(types);
 
   if (!page || page.code !== 200) {
     notFound();
