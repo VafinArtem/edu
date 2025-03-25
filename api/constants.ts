ae14e3a4c412fff2ee084b3c2f689264434b6a71
Vafin Artem <a.vafin85@gmail.com>
2025-03-25 16:58:09 +0500

    Правит страницу новостей

--- api/constants.ts
+++ api/constants.ts
@@ -1,7 +1,7 @@
 export const API = {
   news: {
     all: process.env.BACKEND_API + (process.env.NODE_ENV === "development" ? "/news/" : "/react-api/news/index"),
-    types: process.env.BACKEND_API + (process.env.NODE_ENV === "development" ? "/types/" : "/react-api/news/view"),
+    types: process.env.BACKEND_API + (process.env.NODE_ENV === "development" ? "/types/" : "/react-api/news/type"),
     byAlias: process.env.BACKEND_API + (process.env.NODE_ENV === "development" ? "/one-news/" : "/react-api/news/view"),
   },
   course: {
